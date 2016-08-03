51bec1e30ed985b0b4ee23bc3c52a0aacd10c3d4
josefmadero <josefmadero@gmail.com>
2016-08-03 02:57:57 -0300

    crashea upload sin foto y comparte en fb solo lo que escribo (xxq no esta en google play)

---
+++ app/src/main/java/com/jmadero/entregablefirebase/view/PaintsDetailActivity.java
@@ -0,0 +1,13 @@
+package com.jmadero.entregablefirebase.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaintsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paints_detail);
+    }
+}
